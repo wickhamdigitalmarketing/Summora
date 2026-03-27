18512c52e71fd1e8825e8a3e997e19235e9ea0ba
wickhamdigitalmarketing <wickhamdigitalmarketing@gmail.com>
2026-03-27 19:53:43 -0400

    ty test 2

---
+++ app/thank-you-contractor/page.js
@@ -0,0 +1,18 @@
+export default function ThankYouContractor() {
+  return (
+    <main>
+      <section style={{ textAlign: "center", padding: "80px 20px" }}>
+        <h1>Thank You!</h1>
+        <p>Your contractor application has been received.</p>
+        <p>We appreciate your interest in working with Summora Cabinetry.</p>
+        <p>We’ll review your information and reach out if there’s a good fit.</p>
+
+        <div style={{ marginTop: "30px" }}>
+          <a href="/" className="btn-dark">
+            Return to Home
+          </a>
+        </div>
+      </section>
+    </main>
+  );
+}
